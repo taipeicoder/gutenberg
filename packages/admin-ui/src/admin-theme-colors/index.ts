f70d056e9b9c9dcfc5959e5a7730d2bdb6382831
Ashar Fuadi <fushar@gmail.com>
2026-05-21 10:59:59 +0700

    Update fresh, modern, light, midnight bg colors

--- packages/admin-ui/src/admin-theme-colors/index.ts
+++ packages/admin-ui/src/admin-theme-colors/index.ts
@@ -4,15 +4,15 @@ type AdminThemeColors = {
 };
 
 const ADMIN_THEME_COLORS = new Map< string, AdminThemeColors >( [
-	[ 'fresh', { primary: '#3858e9', bg: '#1e1e1e' } ],
-	[ 'modern', { primary: '#3858e9', bg: '#1e1e1e' } ],
-	[ 'midnight', { primary: '#e14d43', bg: '#363b3f' } ],
+	[ 'fresh', { primary: '#3858e9', bg: '#25292b' } ],
+	[ 'modern', { primary: '#3858e9', bg: '#222524' } ],
+	[ 'midnight', { primary: '#e14d43', bg: '#3d4042' } ],
 	[ 'coffee', { primary: '#46403c', bg: '#7c726c' } ],
 	[ 'ocean', { primary: '#627c83', bg: '#5f787f' } ],
 	[ 'blue', { primary: '#096484', bg: '#0e7da4' } ],
 	[ 'ectoplasm', { primary: '#523f6d', bg: '#8468ab' } ],
 	[ 'sunrise', { primary: '#dd823b', bg: '#cc4541' } ],
-	[ 'light', { primary: '#0085ba', bg: '#e5e5e5' } ],
+	[ 'light', { primary: '#0085ba', bg: '#eaeeed' } ],
 ] );
 
 /**
